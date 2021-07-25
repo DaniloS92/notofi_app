20cce574226979e08f55eb0303d0e3878a8cc819
Danilo <danilo.sanchez@farmaciasmia.com>
2021-07-25 02:44:39 -0500

    [UPD] Notificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:chat/routes/routes.dart';
 import 'package:chat/services/auth_service.dart';
 import 'package:chat/services/get_services.dart';
+import 'package:chat/services/push_notifications_services.dart';
 import 'package:chat/services/reservation_services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
   // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
   //   statusBarColor: Colors.transparent,
   // ));
+  WidgetsFlutterBinding.ensureInitialized();
+  await PushNotificationService.initializeApp();
   runApp(MyApp());
 }
 
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Chat App',
+        title: 'Sbelta App',
         initialRoute: 'login',
         routes: appRoutes,
         localizationsDelegates: [
